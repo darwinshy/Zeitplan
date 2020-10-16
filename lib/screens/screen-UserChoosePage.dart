742dc33d9d83d8ad9bfa2110cffcdc512339043e
Reverope <shatish123456789@gmail.com>
2020-10-16 17:09:28 +0530

    Import Organised

--- lib/screens/screen-UserChoosePage.dart
+++ lib/screens/screen-UserChoosePage.dart
@@ -2,8 +2,8 @@ import 'package:Zeitplan/components/animations.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../components/reusables.dart';
-import '../screens/screen-login.dart';
-import '../screens/screen-register.dart';
+import 'screen-login.dart';
+import 'screen-register.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
